18b08ff92a79ce8ade35bddefc9e1265eeb0bf5b
csappancs <adam.csapo@gmail.com>
2023-10-16 20:58:00 +0200

    full navbar with hamburger menu

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../img/logo.png"
 
-export default function Menubar() {
-  return (
-    <div className='menubar'>
-        <div className='menubarlogo'>
-            <img src={Logo} alt="logo" />
-        </div>
-        <div>
+function Navbar() {
+	const navRef = useRef();
 
-        </div>
-    </div>
-  )
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+			<a href="#"><img src={Logo} alt="" /></a>
+			<nav ref={navRef}>
+				<a href="/#">Home</a>
+				<a href="/#">My work</a>
+				<a href="/#">Blog</a>
+				<a href="/#">About me</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
 }
+
+export default Navbar;
