85bf87d0e76c8c65e000301a8a52cf6d3529592c
csappancs <adam.csapo@gmail.com>
2023-10-16 18:57:44 +0200

    navbar begining

---
+++ src/components/Menubar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from "../img/logo.png"
+
+export default function Menubar() {
+  return (
+    <div className='menubar'>
+        <div className='menubarlogo'>
+            <img src={Logo} alt="logo" />
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
