abe245c935b57e061fd0d0aa8ecdd59644b11899
csappancs <adam.csapo@gmail.com>
2023-10-17 20:07:06 +0200

    teszt for autobuild turend on

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,16 @@ export default function About() {
   return (
     <div>
         <h1>About oldal</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, voluptatum!</p>
+        <hr />
+        <h3>Teszt</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, adipisci.</p>
+        <hr />
+        <h3>Teszt</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, adipisci.</p>
+        <hr />
+        <h3>Teszt</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, adipisci.</p>
     </div>
   )
 }
