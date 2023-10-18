d360b2c76decc6546081e5eb92ac4be7c0c5a916
csappancs <adam.csapo@gmail.com>
2023-10-18 19:37:37 +0200

    hero on click event. Nézd át holnap

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,22 @@
 import React from 'react'
 
 export default function Hero() {
+  const welcome = "Welcome on my site"; 
+
+  const onClickEvent = (name, e) => {
+    console.log("Button clicked " + name, e.target);
+  }
+
+
   return (
     <div className='herocont'>
         <div className='herotitle'>
-            <h1>Welcome on my site</h1>
+            <h1>{ welcome }</h1>
         </div>
         <div className='herosubtitle'>
             <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, odit?</h3>
         </div>
-        <button className='hero-button'>
+        <button className='hero-button' onClick={(e) => onClickEvent("Ádám", e)}>
             Nyomd meg most!!!
         </button>
     </div>
